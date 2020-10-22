08fc6e44836864570de4f88f0a58746848974848
mtakahashiif <18286485+mtakahashiif@users.noreply.github.com>
2020-10-22 23:14:53 +0900

    first GitHub Pages

---
+++ docs/assets/js/draw-chart.js
@@ -0,0 +1,64 @@
+function renderChartContainerTemplate(content, chartIndexEntry, chartNumber) {
+    const chartContainerNode = content.querySelector('#chart-container');
+    chartContainerNode.id = 'chart-container-' + chartNumber;
+
+    const chartCaptionNode = content.querySelector('#chart-caption');
+    chartCaptionNode.id = "chart-caption-" + chartNumber;
+    chartCaptionNode.textContent = chartIndexEntry.caption;
+
+    const chartCanvasNode = content.querySelector('#chart-canvas');
+    chartCanvasNode.id = "chart-canvas-" + chartNumber;
+
+    return content;
+}
+
+
+function attachChart(context, chartData) {
+    const myLineChart = new Chart(context, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: chartData[0].series,
+                data: chartData[0].points
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        unit: 'day'
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const parentNode = document.querySelector('#chart-plain');
+    const templateContent = document.querySelector('#chart-container-template').content;
+
+    fetch('assets/chart-index.json')
+        .then(response => response.json())
+        .then(chartIndex => {
+            for (let i = 0; i < chartIndex.length; i++) {
+                const content = document.importNode(templateContent, true);
+                const renderedContent = renderChartContainerTemplate(content, chartIndex[i], i);
+                parentNode.appendChild(renderedContent);
+
+                fetch(chartIndex[i].data_file)
+                    .then(response => response.json())
+                    .then(chartData => {        
+                        const context = document.getElementById("chart-canvas-" + i);
+                        attachChart(context, chartData)
+                    });
+            }
+        });
+});
