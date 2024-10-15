b4dee81053fb7653272c515adc0cf578dc466b75
Wreath Vine <wreath.vine@gmail.com>
2024-10-15 16:39:20 +0900

    ITA v2 用集計定義追加。

--- test-run.sh
+++ test-run.sh
@@ -5,9 +5,12 @@ python3 -m exastics.publish_github_api_releases exastro-suite oase
 python3 -m exastics.publish_github_api_releases exastro-suite Settings-CloudSystemTemplate-1st
 
 python3 -m exastics.publish_dockerhub_api_repository exastro it-automation
+python3 -m exastics.publish_dockerhub_api_repository exastro exastro-it-automation-migration
 
 python3 -m exastics.create_chart_data_github_download_count ./it-automation/github-releases                    ./docs/assets/chart-data/it-automation-github-download-count.json
 python3 -m exastics.create_chart_data_github_download_count ./oase/github-releases                             ./docs/assets/chart-data/oase-github-download-count.json
 python3 -m exastics.create_chart_data_github_download_count ./Settings-CloudSystemTemplate-1st/github-releases ./docs/assets/chart-data/Settings-CloudSystemTemplate-1st-github-download-count.json
 
 python3 -m exastics.create_chart_data_dockerhub_pull_count ./it-automation/dockerhub-repository/ ./docs/assets/chart-data/it-automation-dockerhub-pull-count.json
+
+python3 -m exastics.create_chart_data_dockerhub_pull_count ./exastro-it-automation-migration/dockerhub-repository/ ./docs/assets/chart-data/exastro-it-automation-dockerhub-pull-count.json
